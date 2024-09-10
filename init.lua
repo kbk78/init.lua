f123fcdcdf6229b2bccd8fc9683169de38781431
kbk78 <planetkbk@gmail.com>
2024-09-10 15:54:21 -0500

    bracketshortcut

--- init.lua
+++ init.lua
@@ -15,6 +15,26 @@ vim.opt.hidden=true
 vim.opt.laststatus=2
 
 
+
+-- macro to remove commnnts in idf and change to single line string
+-- let @e = ":s/,/,\\r/g\<CR>"
+-- let @c = ":s/\\n//\<CR>"
+vim.keymap.set('v', '<Leader>s', ":s/,.*/,<CR>gv:s/;.*/;<CR>gv:s/^ */<CR>gv:s/\\n/<CR>gv:s/^/'<CR>gv:s/$/'<CR>", {silent=true})
+vim.keymap.set('v', '<Leader>r', ':s/,/,\\r/g<CR>', {silent=true})
+vim.keymap.set('v', '<Leader>c', ':s/\\n//<CR>', {silent=true})
+
+-- allow the . to execute once for each line of a visual selection
+vim.keymap.set('v', '.', ':normal .<CR>')
+-- "allow macro to execute once for each line of a visual selection
+-- xnoremap Q :'<,'>:normal @q<CR>
+
+-- save on focuslost
+vim.api.nvim_create_autocmd({ "BufLeave", "FocusLost" },
+	{ callback = function() 
+		if vim.bo.modified and not vim.bo.readonly and vim.fn.expand("%") ~= "" and vim.bo.buftype == "" then
+			vim.api.nvim_command('silent update') end
+		end, })
+
 -- navigate splits
 vim.keymap.set('n', '<c-h>', '<c-w>h', {silent=true})
 vim.keymap.set('n', '<c-j>', '<c-w>j', {silent=true})
@@ -37,14 +57,29 @@ vim.keymap.set('n', '<Tab>',':b#<CR>', {silent=true})
 
 
 -- Explore files
-vim.keymap.set('n', '<Leader>e', ':Ex<CR>', {silent=true})
+vim.keymap.set('n', '<Leader>e', ':E<CR>', {silent=true})
 
 -- conflict with splitnavigation
 -- vim.keymap.set('n', '<c-h>',':bn<CR>', {silent=true})
 -- vim.keymap.set('n', '<c-j>',':bp<CR>', {silent=true})
 
--- replace current word with contents of paste buffer
+-- replace current word/selection with contents of paste buffer
 vim.keymap.set('n', '<c-p>', '"_cw"<esc>', {silent=true})
+vim.keymap.set('v', '<c-p>', '"_c"<esc>', {silent=true})
+
+-- Surround/brackets
+vim.keymap.set('v', '"', 'c"<c-r>+"<esc>', {silent=true})
+vim.keymap.set('v', "'", "c'<c-r>+'<esc>", {silent=true})
+vim.keymap.set('v', '{', 'c{<c-r>+}<esc>', {silent=true})
+vim.keymap.set('v', '[', 'c[<c-r>+]<esc>', {silent=true})
+vim.keymap.set('v', '9', 'c(<c-r>+)<esc>', {silent=true})
+vim.keymap.set('n', '<leader>9', 'i()<esc>ba', {silent=true})
+vim.keymap.set('n', '<leader>"', 'i""<esc>ba', {silent=true})
+vim.keymap.set('n', "<leader>'", "i''<esc>ba", {silent=true})
+vim.keymap.set('n', '<leader>[', 'i[]<esc>ba', {silent=true})
+vim.keymap.set('n', '<leader>{', 'i{}<esc>ba', {silent=true})
+
+
 
 -- file shortcuts
 vim.keymap.set('n', '<leader>v', '<cmd>e  ~/AppData/Local/nvim/init.lua<cr>', {silent=true})
@@ -52,8 +87,10 @@ vim.keymap.set('n', '<leader>wk', '<cmd>e ~/personal/career/google.md<cr>', {sil
 vim.keymap.set('n', '<leader>wj', '<cmd>e ~/personal/journal.md<cr>', {silent=true})
 
 -- python comments
-vim.keymap.set('n', ',c', ':s/^/#<CR>:noh<CR>', {silent=true})
-vim.keymap.set('n', ',x', ':s/^#//<CR>:noh<CR>', {silent=true})
+vim.keymap.set({'v','n'}, ',c', ':s/^/#<CR>:noh<CR>', {silent=true})
+vim.keymap.set({'v','n'}, ',x', ':s/^#//<CR>:noh<CR>', {silent=true})
+
+
 
 
 -- stay at current word when using star search
@@ -61,14 +98,7 @@ vim.keymap.set('n', '*', function() vim.fn.setreg('/', vim.fn.expand('<cword>'))
 
 vim.keymap.set('v','//','y/<C-R>"<CR>', {silent=true})
 
--- save on focuslost
--- :au FocusLost * silent! wa
--- :au FocusLost * echo "Focustlost"
 
--- allow the . to execute once for each line of a visual selection
-vim.keymap.set('v', '.', ':normal .<CR>')
--- "allow macro to execute once for each line of a visual selection
--- xnoremap Q :'<,'>:normal @q<CR>
 
 -- Install lazy.Nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
